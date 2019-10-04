824bc45996e22f91e4d5ce44781b622afc07fe31
Rodrigo García <hola@rodrigogarcia.com.mx>
2019-10-04 15:52:51 -0500

    added first files as commit

---
+++ gulpfile.js
@@ -0,0 +1,202 @@
+/**
+ * MAIN GULPFILE CONFIGURATION
+ * * Is using Sass, purgecss, notyf and rellax
+ */
+const gulp = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'),
+	babel = require('gulp-babel'),
+	concat = require('gulp-concat'),
+	log = require('./logger'),
+	cleanCSS = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	uglify = require('gulp-uglify'),
+	purgecss = require('gulp-purgecss');
+
+////////////////////////
+//* VENDORS VARIABLES //
+////////////////////////
+const $cssVendorsFolder = './public/css/vendors',
+	$notyfCSS = './node_modules/notyf/notyf.min.css',
+	$tailwind = './node_modules/tailwindcss/dist/tailwind.css',
+	$cssVendors = [$tailwind, $notyfCSS],
+	$cssAdminVendors = [$tailwind, $notyfCSS],
+	$jsVendorsFolder = './public/js/vendors',
+	$notyfJS = './node_modules/notyf/notyf.min.js',
+	$editorJS = './node_modules/@editorjs/editorjs/dist/editor.js',
+	$code = './node_modules/@editorjs/code/dist/bundle.js',
+	$delimiter = './node_modules/@editorjs/delimiter/dist/bundle.js',
+	$embed = './node_modules/@editorjs/embed/dist/bundle.js',
+	$header = './node_modules/@editorjs/header/dist/bundle.js',
+	$inlineCode = './node_modules/@editorjs/inline-code/dist/bundle.js',
+	$quote = './node_modules/@editorjs/quote/dist/bundle.js',
+	$simpleImage = './node_modules/@editorjs/simple-image/dist/bundle.js',
+	$table = './node_modules/@editorjs/table/dist/bundle.js',
+	$jsVendors = [
+		$notyfJS,
+		$editorJS,
+		$code,
+		$embed,
+		$header,
+		$quote,
+		$simpleImage,
+		$table,
+		$delimiter,
+		$inlineCode
+	];
+
+/////////////////////
+//! Error Function //
+/////////////////////
+function displayError(error) {
+	log(error.toString(), 'red');
+	this.emit('end');
+}
+
+///////////
+//* SaSS //
+///////////
+gulp.task('sass', () => {
+	return gulp
+		.src('./src/sass/main.scss')
+		.pipe(sourcemaps.init())
+		.pipe(
+			sass({
+				outputStyle: 'compressed'
+			})
+		)
+		.on('error', displayError)
+		.pipe(autoprefixer())
+		.pipe(rename('main.min.css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./public/css'))
+		.on('end', () => {
+			log('SaSS Compiled!', 'green');
+		});
+});
+
+///////////////////////
+//* CSS VENDORS MAIN //
+///////////////////////
+gulp.task('css-vendors-main', () => {
+	return gulp
+		.src($cssVendors, { base: './node_modules' })
+		.pipe(concat('css-main-vendors.css'))
+		.pipe(cleanCSS({ compatibility: 'ie10' }))
+		.pipe(gulp.dest($cssVendorsFolder))
+		.on('end', () => {
+			log('Main Vendors (CSS) Concatenaded and Minified!', 'blue');
+		});
+});
+
+////////////////////////
+//* CSS VENDORS ADMIN //
+////////////////////////
+gulp.task('css-vendors-admin', () => {
+	return gulp
+		.src($cssAdminVendors, { base: './node_modules' })
+		.pipe(concat('css-admin-vendors.css'))
+		.pipe(cleanCSS({ compatibility: 'ie10' }))
+		.pipe(gulp.dest($cssVendorsFolder))
+		.on('end', () => {
+			log('Admin Vendors (CSS) Concatenated and Minified!', 'blue');
+		});
+});
+
+//////////////////////
+//! PURGE EVERY CSS //
+//////////////////////
+gulp.task('purgecss', () => {
+	return gulp
+		.src('public/css/main.min.css', { base: './' })
+		.pipe(
+			purgecss({
+				content: ['src/views/**/*.pug'],
+				whitelist: [
+					'no-scroll',
+					'active',
+					'hide-this',
+					'custom-button-admin',
+					'is-over',
+					'is-over-black',
+					'is-over-gray',
+					'light',
+					'is-input',
+					'is-item',
+					'remove-delay',
+					'welcome',
+					'loading',
+					'animate-section',
+					'codex-editor'
+				]
+			})
+		)
+		.pipe(gulp.dest('./'))
+		.on('end', () => {
+			log('CSS Purge done!', 'purple');
+		});
+});
+
+/////////////////////
+//* JS COMPILATION //
+/////////////////////
+gulp.task('js', () => {
+	return gulp
+		.src('./src/scripts/**/*.js')
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+		.on('error', displayError)
+		.pipe(concat('main.min.js'))
+		.pipe(
+			uglify({
+				mangle: {
+					eval: true
+				}
+			})
+		)
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./public/js'))
+		.on('end', () => {
+			log('JS Compiled and Cocatenated!', 'yellow');
+		});
+});
+
+/////////////////
+//* JS VENDORS //
+/////////////////
+gulp.task('js-vendors', () => {
+	return gulp
+		.src($jsVendors)
+		.pipe(concat('vendors.min.js'))
+		.pipe(gulp.dest($jsVendorsFolder))
+		.on('end', () => {
+			log('JS Vendors Concatenated!', 'yellow');
+		});
+});
+
+///////////////
+//* WATCHERS //
+///////////////
+gulp.task('watchers', done => {
+	log('Watchers Running!', 'yellow');
+	gulp.watch('./src/sass/**/*.scss', gulp.series('sass', 'purgecss'));
+	gulp.watch('./src/scripts/**/*.js', gulp.series('js'));
+	done();
+});
+
+////////////////
+//* MAIN TASK //
+////////////////
+gulp.task(
+	'dev',
+	gulp.series(
+		'sass',
+		'css-vendors-main',
+		'css-vendors-admin',
+		'purgecss',
+		'js',
+		'js-vendors',
+		gulp.parallel('watchers')
+	)
+);
